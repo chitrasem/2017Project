6b8f4872076c470cb3494e37bed2f4be18414376
Sabaychitra84 <sabaychitra84@gmail.com>
2016-12-08 15:35:20 +0700

    Session.java

---
+++ School/src/main/java/com/chitra/school/model/Session.java
@@ -0,0 +1,63 @@
+package com.chitra.school.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import com.chitra.school.utils.StringUtils;
+
+@Entity
+@Table(name="TB_SESSION")
+public class Session {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	private String id;
+	
+	@Column(name="SESSION")
+	private String session;
+	
+	@Column(name="REGISTER_DATE")
+	private String registerDate = StringUtils.getSystemDate();
+	
+	@Column(name="REGISTER_PERSON")
+	private String registerPerson;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSession() {
+		return session;
+	}
+
+	public void setSession(String session) {
+		this.session = session;
+	}
+
+	public String getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(String registerDate) {
+		this.registerDate = registerDate;
+	}
+
+	public String getRegisterPerson() {
+		return registerPerson;
+	}
+
+	public void setRegisterPerson(String registerPerson) {
+		this.registerPerson = registerPerson;
+	}
+	
+	
+	
+
+}
