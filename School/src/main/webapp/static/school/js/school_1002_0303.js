01b5c6453ad3be3c3c951cdd686b3de7dfe609e2
Sabaychitra84 <sabaychitra84@gmail.com>
2017-01-26 09:24:02 +0700

    edited

--- School/src/main/webapp/static/school/js/school_1002_0303.js
+++ School/src/main/webapp/static/school/js/school_1002_0303.js
@@ -2,8 +2,21 @@ $(document).ready(function(){
 	school_1002_0303.loadData();
 });
 var school_1002_0303 = {};
+$("#btnEdit").click(function(){
+	$("#school_1002_0302_form").submit();
+});
 school_1002_0303.loadData = function(input){
 	
 	if(!input) var input = {};
+	
+	$.ajax({
+		url: viewURL,
+		success: function(resp){
+			if(resp.studentRec.length>0){
+				
+			}
+		}
+	});
+	
 	
 };
