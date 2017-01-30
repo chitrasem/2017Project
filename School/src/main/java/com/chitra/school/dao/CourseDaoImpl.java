190057ab35dd6c25a778d146e33216bf6e598c79
Sabaychitra84 <sabaychitra84@gmail.com>
2017-01-30 16:01:25 +0700

    Course

--- School/src/main/java/com/chitra/school/dao/CourseDaoImpl.java
+++ School/src/main/java/com/chitra/school/dao/CourseDaoImpl.java
@@ -2,19 +2,15 @@ package com.chitra.school.dao;
 
 import java.util.List;
 
-import org.apache.xmlbeans.impl.xb.xsdschema.RestrictionDocument.Restriction;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.transform.Transformers;
-import org.hibernate.type.StandardBasicTypes;
-import org.hibernate.type.Type;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.chitra.school.model.Course;
-import com.chitra.school.model.Student;
+import com.chitra.school.utils.AliasToBeanNestedResultTransformer;
 
 
 @Repository("CourseDao")
@@ -42,23 +38,23 @@ public class CourseDaoImpl extends AbstractDao<String, Course> implements Course
 	@SuppressWarnings("unchecked")
 	public List<Course> findAllCourses() {
 		
-		String query = "(SELECT ACY.ACADEMIC_YEAR from TB_ACADEMIC_YEAR ACY where ACY.ID = THIS_.ACY_ID) AS myAlias";
+		//String query = "(SELECT ACY.ACADEMIC_YEAR from TB_ACADEMIC_YEAR ACY where ACY.ID = THIS_.ACY_ID) AS myAlias";
 		
 		Criteria crit = getSession().createCriteria(Course.class, "c");
-		//crit.createAlias("academicYear", "acy");
+		crit.createAlias("academicYear", "acy");
 		crit.setProjection(Projections.projectionList()
 				.add(Projections.property("c.id"), "id")
 				.add(Projections.property("c.course"), "course")
-				/*.add(Projections.property("acy.academicYear"),"academicYear")
-				.add(Projections.property("acy.id"),"id")*/
-				.add(Projections.sqlProjection(query, 
+				.add(Projections.property("acy.academicYear"),"academicYear.academicYear"));
+				//.add(Projections.property("acy.id"),"id")
+			/*	.add(Projections.sqlProjection(query, 
 						new String[]{"myAlias"}, 
 						new Type[] {StandardBasicTypes.STRING}))
 				
-				).setResultTransformer(Transformers.aliasToBean(Course.class));
+				)*/
+		crit.setResultTransformer( new AliasToBeanNestedResultTransformer(Course.class) );
 		crit.add(Restrictions.eq("c.state", "Active"));
 		crit.addOrder(Order.asc("c.course"));
-		crit.setResultTransformer(Transformers.aliasToBean(Course.class));
 		return (List<Course>) crit.list();
 		
 	}
